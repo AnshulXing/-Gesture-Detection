6fba7fdd9af7e180589693928d199610045d9233
AnshulXing <86431802+AnshulXing@users.noreply.github.com>
2021-09-21 21:14:21 +0530

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -1,25 +1,3 @@
-'''
-README:
-# gesture-detection
-
-This project has been built in collaboration with Anshul Singh (https://www.github.com/AnshulXing)
-
-## Setup:
-
-Install cv2, mediapipe and numpy:
-
-Use pip(for python2) or pip3(for python3) to install both the libraries
-
-Commands:<br>
-For open-cv: ```pip3 install opencv-python```<br>
-For mediapipe: ```pip3 install mediapipe```<br>
-For numpy: ```pip3 install numpy```<br>
-
-### For Windows systems, it needs nircmd installed additionally. <br>
-
-Download this zip: http://www.nirsoft.net/utils/nircmd.zip and then copy the nircmd.exe file to C:\\Windows:
-'''
-
 import cv2 as cv
 import mediapipe as mp
 import time
